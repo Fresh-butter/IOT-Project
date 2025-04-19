863f78aae46f6fc383fb09c29bdd97c31af9cbc7
Arushiiiiiyy <arushishukla2007mit@gmail.com>
2025-04-19 13:01:00 +0530

    Update api.js

--- react-frontend/src/services/api.js
+++ react-frontend/src/services/api.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://your-backend-api-url.com';
+const API_BASE_URL = 'https://iot-project-c3wb.onrender.com';
 
 // Example function to fetch live data
 export const getLiveData = async () => {
   try {
-    const response = await axios.get(`${API_BASE_URL}/live-data`);
+    const response = await axios.get(`${API_BASE_URL}/api/routes/`);
     return response.data;
   } catch (error) {
     console.error('Error fetching live data:', error);
